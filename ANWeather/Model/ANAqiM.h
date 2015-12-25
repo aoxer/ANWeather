6c8f135e7c63e2401a1e7143f4ac051b7c8ee672
aoxer <aoxer@163.com>
2015-12-25 16:53:44 +0800

    修改ANAqi模型

---
+++ ANWeather/Model/ANAqiM.h
@@ -0,0 +1,18 @@
+//
+//  ANAqiM.h
+//  ANWeather
+//
+//  Created by a on 15/12/25.
+//  Copyright (c) 2015年 YongChaoAn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ANCity.h"
+
+@interface ANAqiM : NSObject
+/**
+ *  city
+ */
+@property (strong, nonatomic)ANCity *city;
+@end
+
---
+++ ANWeather/Model/ANAqiM.m
@@ -0,0 +1,13 @@
+//
+//  ANAqiM.m
+//  ANWeather
+//
+//  Created by a on 15/12/25.
+//  Copyright (c) 2015年 YongChaoAn. All rights reserved.
+//
+
+#import "ANAqiM.h"
+
+@implementation ANAqiM
+
+@end
