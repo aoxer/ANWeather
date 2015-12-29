9631c06ab47d4d6e6747e18454ba82571d0e7398
aoxer <aoxer@163.com>
2015-12-29 17:33:58 +0800

    完善几日天气的内容

--- ANWeather/Home/View/ANDaysWeatherCell.m
+++ ANWeather/Home/View/ANDaysWeatherCell.m
@@ -41,22 +41,112 @@
 - (void)setDailyForcast:(ANDailyForecastM *)dailyForcast
 {
     _dailyForcast = dailyForcast;
-
+    ANLog(@"%@", dailyForcast.cond.txt_d);
+    self.week.text = [self weekDayFromDate:dailyForcast.date];
     self.date.text = [self dateWithMonthDay:dailyForcast.date];
     self.maxTemp.text = dailyForcast.tmp.max;
     self.minTemp.text = dailyForcast.tmp.min;
-    
-    
+    self.weatherIcon.image = [self getWeatherImageWithCondTxt:dailyForcast.cond.txt_d];
+   
 
 }
 
+/**
+ *  把日期转为月日
+ */
 - (NSString *)dateWithMonthDay:(NSString *)date
 {
+    
     NSString *month = [date substringWithRange:NSMakeRange(5, 2)];
     NSString *day = [date substringWithRange:NSMakeRange(8, 2)];
     NSString *MMDD = [NSString stringWithFormat:@"%@.%@", month, day];
     
     return MMDD;
+}
+
+/**
+ *  根据日期计算星期几
+ */
+- (NSString *)weekDayFromDate:(NSString *)date
+{
+#warning 垃圾代码 待修缮
+    NSString *year = [date substringWithRange:NSMakeRange(0, 4)];
+    NSString *month = [date substringWithRange:NSMakeRange(5, 2)];
+    NSString *day = [date substringWithRange:NSMakeRange(8, 2)];
+    
+    NSDateComponents *comps = [[NSDateComponents alloc] init];
+    [comps setDay:day.integerValue];
+    [comps setMonth:month.integerValue];
+    [comps setYear:year.integerValue];
+    NSCalendar *gregorian = [[NSCalendar alloc]
+                             initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDate *NewDate = [gregorian dateFromComponents:comps];
+    NSDateComponents *weekdayComponents =
+    [gregorian components:NSWeekdayCalendarUnit fromDate:NewDate];
+    NSInteger weekday = [weekdayComponents weekday];
+   
+    switch (weekday) {
+        case 1:
+            return @"周一";
+            break;
+        case 2:
+            return @"周二";
+            break;
+        case 3:
+            return @"周三";
+            break;
+        case 4:
+            return @"周四";
+            break;
+        case 5:
+            return @"周五";
+            break;
+        case 6:
+            return @"周六";
+            break;
+        case 7:
+            return @"周日";
+        default:
+            break;
+    }
+    
+    return @"周日";
+    
+}
+
+/**
+ *  根据天气换图片
+ */
+- (UIImage *)getWeatherImageWithCondTxt:(NSString *)txt
+{
+    
+    
+    if ([txt isEqualToString:@"雷阵雨"]) {
+        return [UIImage imageNamed:@"thunder_mini"];
+        
+    }else if ([txt isEqualToString:@"晴"]){
+        return [UIImage imageNamed:@"sun_mini"];
+        
+    }else if ([txt isEqualToString:@"多云"]){
+        return [UIImage imageNamed:@"sun_and_cloud_mini"];
+        
+    }else if ([txt isEqualToString:@"阴"]){
+        return [UIImage imageNamed:@"cloud"];
+        
+    }else if ([txt hasSuffix:@"雨"]){
+        return [UIImage imageNamed:@"rain_mini"];
+        
+    }else if ([txt hasSuffix:@"雪"]){
+        return [UIImage imageNamed:@"snow_heavyx_mini"];
+        
+    }else if ([txt hasSuffix:@"霾"] || [txt hasSuffix:@"雾"]){
+            return [UIImage imageNamed:@"sand_float_mini"];
+    }else{
+        return [UIImage imageNamed:@"sandfloat_mini"];
+    }
+    
+    return [UIImage imageNamed:@"sun"];
+    
 }
 
 /**
